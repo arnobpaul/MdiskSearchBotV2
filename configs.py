a73c001fb3ca28b1a6be5e14b080ead92612c77b
ARNABPAL973 <117028354+ARNABPAL973@users.noreply.github.com>
2022-12-24 15:02:40 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -29,7 +29,7 @@ Jᴜsᴛ Tʏᴘᴇ ᴀ Mᴏᴠɪᴇ Nᴀᴍᴇ 🎬**""" )
 ɪ ᴄᴀɴ ᴘʀᴏᴠɪᴅᴇ ᴛʜᴇʀᴇ ʏᴏᴜʀ ʟɪɴᴋꜱ.
 
 ꜰᴏʀ ᴍᴏʀᴇ ɪɴꜰᴏ ᴄʟɪᴄᴋ ᴏɴ ʜᴇʟᴘ ✅""" )
-    UPDATES_CHANNEL = os.getenv("UPDATES_CHANNEL", "-1001818160539")
+    UPDATES_CHANNEL = os.getenv("UPDATES_CHANNEL", "-1001824048080")
     DATABASE_URL = os.getenv("DATABASE_URL", "mongodb+srv://lol:lol@cluster0.epmby6x.mongodb.net/?retryWrites=true&w=majority")
     LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", "-1001815706103"))
     RESULTS_COUNT = int(os.getenv("RESULTS_COUNT", 20))
